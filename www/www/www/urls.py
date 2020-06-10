5b879c5980c5cf75193b086700175ea793903393
Silmaen <genteur.slayer@laposte.net>
2020-06-10 11:42:42 +0200

    adding robots.txt the sites

--- www/www/www/urls.py
+++ www/www/www/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
     path('', include('news.urls')),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
