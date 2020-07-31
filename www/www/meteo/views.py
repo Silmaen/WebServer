7054c074476bd94032ad7281acdb52cb6b514ee8
Silmaen <genteur.slayer@laposte.net>
2020-07-31 13:40:52 +0200

    first summary

--- www/www/meteo/views.py
+++ www/www/meteo/views.py
@@ -2,7 +2,9 @@
 from django.shortcuts import render
 from .models import MeteoValue
 import pytz, datetime
+
 tz = pytz.timezone("Europe/Paris")
+
 
 def get_data(last):
     """
@@ -41,6 +43,42 @@ def smooth_data(data, smooth_width):
     return out
 
 
+class displaydata:
+    def __init__(self, t=0, tt="", h=0, ht=""):
+        self.temperature = str(t)
+        self.temp_tendance = tt
+        self.humidity = str(h)
+        self.hum_tendance = ht
+
+    def compute_from_data(self, dta, dha):
+        ma = max(dta)
+        mi = min(dta)
+        if abs(mi - dta[0]) < 0.2 and abs(ma - dta[-1]) < 0.2:
+            self.temp_tendance = "mdi-arrow-up-bold-outline tred"
+        elif abs(ma - dta[0]) < 0.2 and abs(mi - dta[-1]) < 0.2:
+            self.temp_tendance = "mdi-arrow-down-bold-outline tblue"
+        elif dta[0] > dta[-1]:
+            self.temp_tendance = "mdi-arrow-top-right-bold-outline torange"
+        elif dta[-1] > dta[0]:
+            self.temp_tendance = "mdi-arrow-bottom-right-outline tlightblue"
+        else:
+            self.temp_tendance = "mdi-arrow-left-right-outline tgreen"
+        self.temperature = "{:.2f}".format(dta[-1])
+        ma = max(dha)
+        mi = min(dha)
+        if abs(mi - dha[0]) < 0.2 and abs(ma - dha[-1]) < 0.2:
+            self.hum_tendance = "mdi-arrow-up-bold-outline tred"
+        elif abs(ma - dha[0]) < 0.2 and abs(mi - dha[-1]) < 0.2:
+            self.hum_tendance = "mdi-arrow-down-bold-outline tblue"
+        elif dta[0] > dha[-1]:
+            self.hum_tendance = "mdi-arrow-top-right-bold-outline torange"
+        elif dta[-1] > dha[0]:
+            self.hum_tendance = "mdi-arrow-bottom-right-outline tlightblue"
+        else:
+            self.hum_tendance = "mdi-arrow-left-right-outline tgreen"
+        self.humidity = "{:.2f}".format(dha[-1])
+
+
 def index(request):
     smoo = 0
     ll = "3days"
@@ -51,7 +89,6 @@ def index(request):
                 smoo = 0
         except:
             smoo = 0
-
         try:
             ll = request.POST.get("i_last")
             print(ll)
@@ -72,10 +109,16 @@ def index(request):
         dates.append(str(sset.date))
         temperatures.append(sset.server_room_temperature)
         humidity.append(sset.server_room_humidity)
+    d = displaydata()
+    if len(temperatures)> 20:
+        d.compute_from_data(temperatures[-20:], humidity[-20:])
+    else:
+        d.compute_from_data(temperatures, humidity)
     return render(request, "baseMeteo.html",
-                  {'dates': dates,
+                  {'dates'       : dates,
                    'temperatures': temperatures,
-                   'humidity': humidity,
-                   'smoothing': smoo,
-                   'last': ll,
+                   'humidity'    : humidity,
+                   'smoothing'   : smoo,
+                   'last'        : ll,
+                   'current'     : d,
                    })
