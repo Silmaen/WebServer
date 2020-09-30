72c1b05f9381edb68aab43597c0a933ec420b6e6
Silmaen <genteur.slayer@laposte.net>
2020-09-30 13:05:21 +0200

    some hotfixes

--- www/www/sysadmin/views.py
+++ www/www/sysadmin/views.py
@@ -1,12 +1,6 @@
+"""sysadmin.view"""
 from news.render_utils import render_page
-
-# Create your views here.
-
-subpages = [
-    {"url": "summary", "name": "Summary", "icon": "mdi mdi-home"},
-    {"url": "mlist", "name": "Machine list", "icon": "mdi mdi-desk"},
-]
-
+from .admin_utils import get_ActiveMachine
 
 def index(request):
     return summary(request)
@@ -17,5 +11,9 @@ def summary(request):
 
 
 def mlist(request):
-    return render_page(request, "ServerAdmin", {"subpage": "Liste Machine"})
+    machines = get_ActiveMachine()
+    return render_page(request, "ServerAdmin", {
+        "subpage": "Liste Machine",
+        "machines": machines
+    })
 
