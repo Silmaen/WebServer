28575b426341323ea684eed4e9451a0fc9d070ea
Silmaen <genteur.slayer@laposte.net>
2020-11-04 16:20:17 +0100

    update the static files for markdownx

--- drone/drone/drone/settings.py
+++ drone/drone/drone/settings.py
@@ -163,9 +163,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
