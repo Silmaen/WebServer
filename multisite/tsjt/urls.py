1c6d5e61078730a5df5e3d1f4f7169d7ee1dd272
Silmaen <genteur.slayer@laposte.net>
2020-11-17 21:59:45 +0100

    url hotfix for test subject

--- multisite/tsjt/urls.py
+++ multisite/tsjt/urls.py
@@ -1,18 +1,15 @@
 """Fichier main.urls.py définissant les urls."""
-from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings as main_settings
-from django.urls import path, include
+from django.urls import path
 from .views import *
 
 
 urlpatterns = [
-    path("", index),
-    path('tsjt/', index, name='index'),
+    path("", index, name='index'),
+    path('tsjt/', index, name='index1'),
     path('article/<int:id>-<slug:slug>', lire, name='lire'),
-    path('about', about),
-    path('blog', blog),
-    path('medium', media),
-    path('admin/', admin.site.urls),
-    path('markdownx/', include('markdownx.urls')),  # Pour le décodage de Markdown.
+    path('about', about, name='about'),
+    path('blog', blog, name='blog'),
+    path('medium', media, name='media'),
 ] + static(main_settings.MEDIA_URL, document_root=main_settings.MEDIA_ROOT)
